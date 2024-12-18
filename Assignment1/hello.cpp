c818d2b5c00cd4732dce8fdf0fce8100773255fb
Mobina Ahmadimasoud <st067173@student.spbu.ru>
2024-12-18 23:09:01 +0300

    Update hello.cpp

---
+++ Assignment1/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+std::string hello() {
+    setlocale(LC_ALL, "English");
+    std::cout << __cplusplus << std::endl;
+    std::string input;
+    std::getline(std::cin, input);
+    return input;
+}
