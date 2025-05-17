7028232dc3e171a9a07206c922f9299ae63e07d6
Mobina Ahmadimasoud <st067173@student.spbu.ru>
2025-05-17 18:39:25 +0300

    Update main.cpp

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -5,10 +5,24 @@
 
 int main()
 {
+    // Creates an "AKM" gun object with name "AKM", damage 30, and ammo 90
     AKM akm = AKM("AKM", 30, 90);
+    
+    // Creates a "Transformer" character with:
+    // - Name: "Transformer"
+    // - Type: "Divine"
+    // - Health: 200
+    // - Gun details taken from 'akm' (type, damage, ammo)
+    // - Additional parameter: 100 (could be armor, energy, etc.)
     Transformer transformer = Transformer("Transformer", "Divine", 200,
                                           akm.get_gun_type(), akm.get_damage(), akm.get_ammo(), 100);
 
+    // Creates a "Shooter" character with:
+    // - Name: "Shooter"
+    // - Type: "Autobot"
+    // - Health: 100
+    // - Gun details taken from 'akm' (type, damage, ammo)
+    // - Additional parameter: 100 (could be speed, shield, etc.)
     Shooter shooter = Shooter("Shooter", "Autobot", 100,
                               akm.get_gun_type(), akm.get_damage(), akm.get_ammo(), 100);
 
