b3212b3b5c9cdb0c641e1f5a4473052b55a0bf44
Mobina Ahmadimasoud <st067173@student.spbu.ru>
2025-05-18 18:45:02 +0300

    Update sniper.cpp

--- Assignment 3/sniper.cpp
+++ Assignment 3/sniper.cpp
@@ -19,33 +19,42 @@ Sniper::Sniper(const std::string &name, const std::string &fraction, const int &
 }
 
 
+// Activates sniper scope (zooms in for precise shots)  
+// Returns true if successful
 bool Sniper::scope() {
     std::cout << "Sniper use scope" << std::endl;
     return true;
 }
 
+// Enables camouflage (reduces enemy detection range)
 bool Sniper::hide() {
     std::cout << "Sniper use camouflage" << std::endl;
     return true;
 }
 
+// Ultimate ability: Headshot (high-damage precision shot)  
+// Overrides Transformer/Shooter's ultimate (polymorphism)
 bool Sniper::ultimate() {
     std::cout << get_name() << " take a breath and shoot headshot" << std::endl;
     return true;
 }
 
+// Returns current "mind control" value
 unsigned int Sniper::get_mind_control() {
     return _mind_control;
 }
 
+// Updates mind control
 void Sniper::set_mind_control(unsigned int mind_control) {
     this->_mind_control = mind_control;
 }
 
+// Returns ultimate charge progress
 unsigned int Sniper::get_points_to_ulta() {
     return _points_to_ulta;
 }
 
+// Sets ultimate charge
 void Sniper::set_points_to_ulta(unsigned int points_to_ulta) {
     this->_points_to_ulta = points_to_ulta;
 }
