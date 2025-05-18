01c2fe55ab82234da238232bfd42a186b9069239
Mobina Ahmadimasoud <st067173@student.spbu.ru>
2025-05-18 19:01:20 +0300

    Update sniper.h

--- Assignment 3/sniper.h
+++ Assignment 3/sniper.h
@@ -10,24 +10,29 @@ public:
 
     ~Sniper() = default;
 
+    // Static Methods
+    // Static because scoping behavior is universal to all snipers
     static bool scope();
 
     static bool hide();
 
     bool ultimate() override;
 
+    // Gets current focus level
     unsigned int get_mind_control();
 
+    // Sets focus level
     void set_mind_control(unsigned int mind_control);
 
+    // Gets ultimate charge progress
     unsigned int get_points_to_ulta();
 
-
+    // Sets ultimate charge
     void set_points_to_ulta(unsigned int points_to_ulta);
 
 private:
-    unsigned int _mind_control{};
-    unsigned int _points_to_ulta{};
+    unsigned int _mind_control{};  // Aim stability metric
+    unsigned int _points_to_ulta{};  // Ultimate ability charge
 };
 
 
