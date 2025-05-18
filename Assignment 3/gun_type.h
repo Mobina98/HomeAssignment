bdd5e016b1aa5f7d4d5453d8fdddb2b87917598b
Mobina Ahmadimasoud <st067173@student.spbu.ru>
2025-05-18 15:28:39 +0300

    Update gun_type.h

--- Assignment 3/gun_type.h
+++ Assignment 3/gun_type.h
@@ -2,30 +2,39 @@
 #define GUN_TYPE_H
 
 
+// Constructor
 class Gun {
 public:
     Gun(const std::string &gun_type, const unsigned int &damage, const unsigned int &ammo);
 
+    // Default destructor
     ~Gun() = default;
 
+    // Returns the gun's model
     std::string get_gun_type() const;
 
+    // Sets the gun type
     void set_gun_type(std::string gun_type);
 
+    // Returns current ammo count
     unsigned int get_ammo() const;
 
+    // Updates ammo count
     void set_ammo(const unsigned int ammo);
 
+    // Returns damage per shot
     unsigned int get_damage() const;
 
+    // Updates damage value
     void set_damage(const unsigned int damage);
 
-protected:
+protected:  // Accessible by derived classes
     std::string _gun_type;
     unsigned int _ammo;
     unsigned int _damage;
 };
 
+// AKM - A type of assault rifle (inherits from Gun)
 class AKM : public Gun {
 public:
     AKM(const std::string &name, const unsigned int &damage, const unsigned int &ammo);
@@ -33,6 +42,7 @@ public:
     ~AKM() = default;
 };
 
+// AWP - A high-power sniper rifle
 class AWP : public Gun {
 public:
     AWP(const std::string &name, const unsigned int &damage, const unsigned int &ammo);
@@ -40,6 +50,7 @@ public:
     ~AWP() = default;
 };
 
+// Bazooka - Explosive projectile weapon
 class Bazooka : public Gun {
 public:
     Bazooka(const std::string &name, const unsigned int &damage, const unsigned int &ammo);
@@ -54,4 +65,5 @@ public:
     ~Arm() = default;
 };
 
+// Prevents multiple inclusions of this file
 #endif //GUN_TYPE_H
